e8955e8315036b63a14be829c66a22b628bc9976
Nevenka Lukic <nenamilkov@gmail.com>
2026-06-25 14:27:41 +0200

    Use generic credential placeholder

--- capture_changes.py
+++ capture_changes.py
@@ -1,7 +1,7 @@
 import dlt
 from pg_replication import replication_resource
 
-CREDENTIALS = "postgresql://postgres:postgres@localhost:5432/appdb"
+CREDENTIALS = "postgresql://your_user@localhost:5432/your_db"
 
 pipeline = dlt.pipeline(
     pipeline_name="cdc_pipeline",
--- pipeline.py
+++ pipeline.py
@@ -2,7 +2,7 @@ import dlt
 from pg_replication import replication_resource
 from pg_replication.helpers import init_replication
 
-CREDENTIALS = "postgresql://postgres:postgres@localhost:5432/appdb"
+CREDENTIALS = "postgresql://your_user@localhost:5432/your_db"
 SLOT_NAME = "cdc_pipeline_slot"
 PUB_NAME = "cdc_pipeline_pub"
 
